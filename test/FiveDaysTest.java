0e0aa00ed531710ac1817f6e64c5bf7f64c38efd
19NikaV04 <vasileva.s.veronika@gmail.com>
2024-09-14 22:10:12 +0300

    Initial commit

---
+++ test/FiveDaysTest.java
@@ -0,0 +1,60 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.igor.Weather.Weather;
+
+import static io.restassured.RestAssured.given;
+
+public class FiveDaysTest extends AccuweatherAbstractTest {
+
+    @Test
+    void test5DaysSamara(){
+        Weather weather = given().queryParam("apikey", getApiKey())
+                .when()
+                .get(getBaseUrl() + "/forecasts/v1/daily/5day/290396")
+                .then()
+                .statusCode(200)
+                .extract()
+                .body().as(Weather.class);
+
+        Assertions.assertEquals(5,weather.getDailyForecasts().size());
+    }
+
+    @Test
+    void test5DaysMoscow(){
+        Weather weather = given().queryParam("apikey", getApiKey())
+                .when()
+                .get(getBaseUrl() + "/forecasts/v1/daily/5day/294021")
+                .then()
+                .statusCode(200)
+                .extract()
+                .body().as(Weather.class);
+
+        Assertions.assertEquals(5,weather.getDailyForecasts().size());
+    }
+
+    @Test
+    void test5DaysSaintPetersburg(){
+        Weather weather = given().queryParam("apikey", getApiKey())
+                .when()
+                .get(getBaseUrl() + "/forecasts/v1/daily/5day/295212")
+                .then()
+                .statusCode(200)
+                .extract()
+                .body().as(Weather.class);
+
+        Assertions.assertEquals(5,weather.getDailyForecasts().size());
+    }
+
+    @Test
+    void test5DaysKazan(){
+        Weather weather = given().queryParam("apikey", getApiKey())
+                .when()
+                .get(getBaseUrl() + "/forecasts/v1/daily/5day/295954")
+                .then()
+                .statusCode(200)
+                .extract()
+                .body().as(Weather.class);
+
+        Assertions.assertEquals(5,weather.getDailyForecasts().size());
+    }
+}
